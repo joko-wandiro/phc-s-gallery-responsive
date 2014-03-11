559de0f298f116d9e03ff1c3bcfd282891333ce5
remobius <joko_wandiro@yahoo.com>
2014-03-11 12:42:48 +0700

    Update

--- inc/pages/documentation.php
+++ inc/pages/documentation.php
@@ -16,24 +16,26 @@ function phc_s_gallery_responsive_documentation_page(){
 	PHC_S_GALLERY_RESPONSIVE_PATH_URL . "js/documentation/documentation.js", array("jquery-ui-tabs"));
 ?>
 	<div class="wrap" id="<?php echo PHC_S_GALLERY_RESPONSIVE_IDENTIFIER; ?>">
-	<?php screen_icon('page'); ?>
-	<h2><?php _e("Documentation", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?></h2>
-	
-	<div id="container-donate-btn">
-	<div class="form">
+	<!-- Start Banner -->
+	<ul id="banners">
+	<li>
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 	<input type="hidden" name="cmd" value="_s-xclick">
 	<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHRwYJKoZIhvcNAQcEoIIHODCCBzQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC82C6M8e8Rs3FwEhEfVqnAkOF1446MTD9eC94IdP9I59/SxrhgWsOb3uxl92nftFm3I2frn0FWTCe6dnbc0g3RdQ5S7kPhZUoLl5Nm+Bu2GMhkwa0nwkkrRV0zob2XSfXjp25azZGjQP84T9VQa0qYl93E5gQcVaRj1cjZPRpaGjELMAkGBSsOAwIaBQAwgcQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI/yd7F89pgOSAgaBbX22jgJe6pIKhgI/zT/xGeA2NvVdagj+fY9pzXFBz2TuEWrXDRNPgY1v2Ysac/+XfAU6VETQOZp/Jk1zhlsLpCXYQn+8MOSx0aZ9M7MivBQJQqXnhij0NPiJSEsvDgeb/VaKmVAghKtFx8NU8t/z7l31q2fW5PyxwInLyn3JbeKBcuFLs+BL2B55bz37trycWQyXAdH71PjKKuA8l+ES5oIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTMwOTE5MTYxNzM2WjAjBgkqhkiG9w0BCQQxFgQU91vWXHKz9VO4gY89n9HfHpayolYwDQYJKoZIhvcNAQEBBQAEgYCpj5eWH2A+rqAGglxeJ7AMumxAC0q83P5qqvqkAg11e6ZH9DI0nkZCh9H58D9ZHh47YDtVJ6Km32yManEwfPxJlFJwKbPxKhYe+eLF+owlp/BeT6d080ZMNJQmFQnPSAJLe37kqGF+HxpacPhBPoRtLYyrEcAO4AvP5xEUgW/sOg==-----END PKCS7-----
 	">
-	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	<input type="image" src="<?php echo PHC_S_GALLERY_RESPONSIVE_BANNER . "banner_donation.png"; ?>" border="0" name="submit" 
+	alt="Help me with your Donation !">
 	<img alt="" border="0" src="https://www.paypalobjects.com/id_ID/i/scr/pixel.gif" width="1" height="1">
 	</form>
-	</div>
-	<div class="float-left">
-	<?php _ex("Your donation keep me to maintain and make it free of bug.", 
-	"donation text", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?>
-	</div>
-	</div>
+	</li>
+	<li class="or"><img src="<?php echo PHC_S_GALLERY_RESPONSIVE_BANNER . "banner_or.png"; ?>" /></li>
+	<li><a href="http://www.opencart.com/index.php?route=extension/extension&filter_username=phantasmacode"><img src="<?php echo PHC_S_GALLERY_RESPONSIVE_BANNER . "banner_opencart.png"; ?>" /></a></li>
+	</ul>
+	<!-- End Banner -->
+		
+	<?php screen_icon('page'); ?>
+	<h2><?php _e("Documentation", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?></h2>
+	
 	<div id="tabs">
 	<ul>
 	<li><a href="#tabs-how-to-use"><?php _ex("How to Use", "documentation tab", 
@@ -127,7 +129,7 @@ function phc_s_gallery_responsive_documentation_page(){
 	<p><?php _e("Goto Appearance &gt; Widgets.", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?></p>
 	</li>
 	<li>
-	<p><?php _e("Use Widget Text. Enter Title. Ex: S Gallery Responsive Widget. Then add shortcode into content. You should add parameter type=&quot;widget&quot; into shortcode. See the following image.", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?></p>
+	<p><?php _e("Use Widget Text. Enter Title. Ex: S Gallery Responsive Widget. Then add shortcode for widget into content / textarea.", PHC_S_GALLERY_RESPONSIVE_IDENTIFIER); ?></p>
 	<p>
 	<img src="<?php echo PHC_S_GALLERY_RESPONSIVE_IMG_DOCUMENTATION_BOW_TO_USE_URL . "step11.png"; ?>" 
 	class="step11" />
